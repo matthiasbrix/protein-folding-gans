b8a5375bbaa155d844ea5603e032fc7df048cb95
Matthias Brix <brixmatthias@gmail.com>
2019-09-25 18:06:54 +0200

    add openprotein enabling to read the protein data

--- src/dataloader.py
+++ src/dataloader.py
@@ -2,9 +2,10 @@ import torch
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 import numpy as np
+from openprotein.util import contruct_dataloader_from_disk
 
 class DataLoader():
-    def __init__(self, directories, batch_size, dataset):
+    def __init__(self, directories, batch_size, dataset, training_file=None, validation_file=None, residue_fragments=128, atom=None):
         self.directories = directories
         self.data = None
         self.n_classes = None
@@ -24,24 +25,32 @@ class DataLoader():
                 [transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
             ), train=True, download=True)
             test_set = datasets.MNIST(root=self.root, transform=transforms.ToTensor(), train=False, download=True)
+            self._set_data_loader(train_set, test_set)
+        elif dataset.lower() == "proteins":
+            # Takes a matrix N x 3
+            self.train_loader = contruct_dataloader_from_disk(training_file, batch_size, drop_last=True)
+            self.validation_loader = contruct_dataloader_from_disk(validation_file, batch_size, drop_last=True)
+            self.img_dims = (residue_fragments, residue_fragments)
+            self.residue_fragments = residue_fragments # TODO: maybe not needed.....
+            self.atom = atom
+            self.num_val_batches = len(self.validation_loader)
+            self.num_val_samples = self.validation_loader.dataset.__len__() # self.num_val_batches*self.batch_size
         else:
             raise ValueError("DATASET N/A!")
 
+        self.with_labels = dataset not in ["proteins"]
         self.input_dim = np.prod(self.img_dims)
-        self.with_labels = True
         self.dataset = dataset
 
-        self._set_data_loader(train_set, test_set)
         self.num_train_batches = len(self.train_loader)
-        self.num_test_batches = len(self.test_loader)
-        # could also call len(self.train_loader.dataset) but is more flexible this way
-        self.num_train_samples = self.num_train_batches*self.batch_size
-        self.num_test_samples = self.num_test_batches*self.batch_size
+        self.num_train_samples = self.train_loader.dataset.__len__() # self.num_train_batches*self.batch_size
+
+        print(self.img_dims, self.input_dim, len(self.train_loader), self.num_train_batches, self.num_train_samples, self.num_val_batches, self.num_val_samples)
 
     def _set_data_loader(self, train_set, test_set):
         self.train_loader = torch.utils.data.DataLoader(dataset=train_set,\
             batch_size=self.batch_size, drop_last=True, shuffle=True)
-        self.test_loader = torch.utils.data.DataLoader(dataset=test_set,\
+        self.validation_loader = torch.utils.data.DataLoader(dataset=test_set,\
             batch_size=self.batch_size, drop_last=True, shuffle=True)
 
     def get_new_test_data_loader(self):
