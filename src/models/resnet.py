acf7953419e4e3c2c52b79b1d44f14a6e47903f5
Matthias Brix <brixmatthias@gmail.com>
2019-11-17 18:42:38 +0100

    checkpoint

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -1,9 +1,24 @@
+import torch
 import torch.nn as nn
+
+from functools import partial
 
 class Conv2dAuto(nn.Conv2d):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.padding = (self.kernel_size[0] // 2, self.kernel_size[1] // 2) # dynamic add padding based on the kernel_size
+
+conv3x3 = partial(Conv2dAuto, kernel_size=3, bias=False)
+conv = conv3x3(in_channels=32, out_channels=64)
+
+def activation_func(activation):
+    print("her", activation)
+    return  nn.ModuleDict([
+        ['relu', nn.ReLU(inplace=True)],
+        ['leaky_relu', nn.LeakyReLU(negative_slope=0.01, inplace=True)],
+        ['selu', nn.SELU(inplace=True)],
+        ['none', nn.Identity()]
+    ])[activation]
 
 # The residual block takes an input with in_channels, applies some blocks of convolutional layers
 # to reduce it to out_channels and sum it up to the original input
@@ -14,37 +29,74 @@ class ResidualBlock(nn.Module):
         self.in_channels, self.out_channels, self.activation = in_channels, out_channels, activation
         self.blocks = nn.Identity()
         self.activate = activation_func(activation)
-        self.shortcut = nn.Identity()   
+        self.shortcut = nn.Identity()
     
     def forward(self, x):
         residual = x
         if self.should_apply_shortcut:
             residual = self.shortcut(x)
         x = self.blocks(x)
+        print(x, self.should_apply_shortcut)
         x += residual
+        print(x)
         x = self.activate(x)
+        print(x)
         return x
     
     @property
     def should_apply_shortcut(self):
         return self.in_channels != self.out_channels
 
+class ResNetResidualBlock(ResidualBlock):
+    def __init__(self, in_channels, out_channels, expansion=1, downsampling=1, conv=conv3x3, *args, **kwargs):
+        super().__init__(in_channels, out_channels, *args, **kwargs)
+        self.expansion, self.downsampling, self.conv = expansion, downsampling, conv
+        self.shortcut = nn.Sequential(
+            nn.Conv2d(self.in_channels, self.expanded_channels, kernel_size=1,
+                      stride=self.downsampling, bias=False),
+            nn.BatchNorm2d(self.expanded_channels)) if self.should_apply_shortcut else None
+        
+    @property
+    def expanded_channels(self):
+        return self.out_channels * self.expansion
+    
+    @property
+    def should_apply_shortcut(self):
+        return self.in_channels != self.expanded_channels
 
-def activation_func(activation):
-    return  nn.ModuleDict([
-        ['relu', nn.ReLU(inplace=True)],
-        ['leaky_relu', nn.LeakyReLU(negative_slope=0.01, inplace=True)],
-        ['selu', nn.SELU(inplace=True)],
-        ['none', nn.Identity()]
-    ])[activation]
 
+def conv_bn(in_channels, out_channels, conv, *args, **kwargs):
+    return nn.Sequential(conv(in_channels, out_channels, *args, **kwargs), nn.BatchNorm2d(out_channels))
+
+class ResNetBasicBlock(ResNetResidualBlock):
+    """
+    Basic ResNet block composed by two layers of 3x3conv/batchnorm/activation
+    """
+    expansion = 1
+    def __init__(self, in_channels, out_channels, *args, **kwargs):
+        super().__init__(in_channels, out_channels, *args, **kwargs)
+        self.blocks = nn.Sequential(
+            conv_bn(self.in_channels, self.out_channels, conv=self.conv, bias=False, stride=self.downsampling),
+            activation_func(self.activation),
+            conv_bn(self.out_channels, self.expanded_channels, conv=self.conv, bias=False),
+        )
+    
+# TODO: 1. read article of ultra deep (focus on residual block/archs)
+# TODO: 2. read DESTINI article (focus on residual blocks/archs)
+# TODO: 3 try implementing an architecture...
 
 if __name__ == "__main__":
-    #conv3x3 = partial(Conv2dAuto, kernel_size=3, bias=False)
-    #conv = conv3x3(in_channels=32, out_channels=64)
-    ResidualBlock(32, 64)
-
+    a = ResidualBlock(32, 64)
+    print("a", a)
     dummy = torch.ones((1, 1, 1, 1))
+    dummy += 5
     block = ResidualBlock(1, 64)
     res = block(dummy)
+    print("result!", res)
+    asd = ResNetResidualBlock(32, 64)
+    print(asd)
+    dummy = torch.ones((1, 32, 224, 224))
+    block = ResNetBasicBlock(32, 64)
+    res = block(dummy).shape
+    print(block)
     print(res)
